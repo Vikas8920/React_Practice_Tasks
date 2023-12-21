0bcf4e42e21d7fc17f62c7f21bbf8931d5f13a53
Vikas8920 <vikasmishra0306@gmail.com>
2023-12-21 20:50:49 +0530

    Task-1

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
 import './App.css'
-import Form from './Components/Form';
-import React, {useState} from 'react';
+import Card from './Components/Card';
+import Image1 from './Components/Assets/Images/Web.jpg'
+import Image2 from './Components/Assets/Images/android.jpg'
+import Image3 from './Components/Assets/Images/python.png'
+import Image4 from './Components/Assets/Images/mern.png'
+
+const content1 = 'Web development is the work involved in developing a website for the Internet (World Wide Web) or an intranet (a private network).'
+
+const content2 = 'Android development is a form of software engineering dedicated to creating applications for devices that run on the Android platform.'
+
+const content3 = 'Python is a high-level, general-purpose programming language. Its design code readability with the use of significant indentation.'
+
+const content4 = 'MERN Stack is a combination of the latest technologies used for creating premium web applications.'
 
 function App() {
-  const [form, setForm] = useState(true)
-  const [msg, setMsg] = useState('')
-
-  //Login form
-  function handleForm(){
-    if(form==false){
-      setForm(true)
-      setMsg('Login form shows successfully')
-    }
-  }
-
-  //Registration form
-  function handleForm1(){
-    if(form==true){
-      setForm(false)
-      setMsg('Registration form shows successfully')
-    }
-  }
   return (
      <>
-     
-    <div className='container d-flex justify-content-evenly'>
-      <div className='mt-5'>
-        <button onClick={handleForm} className='btn btn-success mx-4 px-4'>Click for Login form</button>
-        <button onClick={handleForm1} className='btn btn-primary'>Click for Registeration form</button>
-        <h4 className='mt-5 pt-5 ms-4 text-primary'>{msg}</h4>
-      </div>
-      <div>
-        <Form form={form}/>
-      </div>
-    </div>
-    
+       <div className='container'>
+       <h1 className='mt-4'>React Task 1</h1>
+       <div className='row'>
+          <div className='col-md-3'>
+            <Card imgSrc={Image1} title='Web Development' content={content1}/>
+          </div>
+          <div className='col-md-3'>
+            <Card imgSrc={Image2} title='Android Development' content={content2}/>
+          </div>
+          <div className='col-md-3'>
+            <Card imgSrc={Image3} title='Python Development' content={content3}/>
+          </div>
+          <div className='col-md-3'>
+            <Card imgSrc={Image4} title='Mern stack Development' content={content4}/>
+          </div>
+       </div>
+       </div>
      </>
   )
 }
